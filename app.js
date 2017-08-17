2f3915f0addacb740dd4eb36ab800d4fd64343e3
Yev <yev@devatlant.com>
2017-08-17 12:47:37 +0300

    fix response

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 201;
-  res.setHeader('Content-Type', 'text/json');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello lenya\n');
 });
 
